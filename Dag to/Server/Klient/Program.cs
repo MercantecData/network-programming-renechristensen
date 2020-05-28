a44ed2d777fe479b22d464a19ff32d46f349d05b
rene <rene723@gmail.com>
2020-05-28 08:39:54 +0200

    A lot of files that would have been ignored with a gitignore for vs

---
+++ Dag to/Server/Klient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Klient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // basale typer declareres
+            String textToSend = "";
+            int port = 22000;
+
+            // connection relaterede typer declareres
+            TcpClient client = new TcpClient();
+            IPAddress IP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(IP, port);
+            
+
+            //Herefter laves der en reference til datastrømmen kaldet streaming.
+            NetworkStream stream = findConnection(client, endPoint);
+
+            sendingMessages(textToSend, stream);
+
+            client.Close();
+            
+        }
+
+        // client opretter en forbindelse til vores endpoint, altså det device der har ipaddressen IP. Her forbindes den til den process på modtagercomputeren der
+        // har pågældende portnr. I dette tilfælde er det vores serverprogram. 
+        public static NetworkStream findConnection(TcpClient client, IPEndPoint endPoint)
+        {
+
+            client.Connect(endPoint);
+            return client.GetStream();
+        }
+
+        public static void sendingMessages( String textToSend, NetworkStream stream)
+        {
+            int count = 1;
+            while (count <= 5)
+            {
+                Console.WriteLine("What message do you want to send to the server?");
+                textToSend = Console.ReadLine();
+                byte[] textBuffer = Encoding.UTF8.GetBytes(textToSend);
+                stream.Write(textBuffer, 0, textBuffer.Length);
+                count++;
+            }
+        }
+    }
+}
---
+++ Dag to/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace DagtoServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Basic declarations
+            int port = 22000;
+            int bytesInTextReceivedBuffer = 0;
+            byte[] textReceivedBuffer = new byte[1024];
+            String textReceived = "";
+
+            // Declares Ipaddress, Endpoint and TcpClient
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint localIpEndPoint = new IPEndPoint(ip, port);
+            
+
+
+            // Declares tcpListerner and starts listening for incoming connections
+            TcpListener listerner = new TcpListener(localIpEndPoint);
+            listerner.Start();
+
+            
+            Console.WriteLine("Okay so now we just wait for input");
+
+            TcpClient ourClient = listerner.AcceptTcpClient();
+            NetworkStream clientStream = ourClient.GetStream();
+
+            while (textReceived != "End")
+            { 
+                
+                bytesInTextReceivedBuffer = clientStream.Read(textReceivedBuffer, 0, 1024);
+                textReceived = Encoding.UTF8.GetString(textReceivedBuffer, 0, bytesInTextReceivedBuffer);
+
+                Console.WriteLine(textReceived);
+            }
+        }
+    }
+}
