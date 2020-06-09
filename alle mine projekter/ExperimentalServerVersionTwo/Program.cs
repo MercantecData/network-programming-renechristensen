1aaf7b22509b0ef5ea7d32e35e04ae20d28f348c
rene <rene723@gmail.com>
2020-06-09 13:24:28 +0200

    Moving every projekt to gitkracken in order to change computers. Cleanup

---
+++ alle mine projekter/ExperimentalServerVersionTwo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ExperimentalServerVersionTwo
+{
+    class MainClass
+    {
+        static String ServerSays = "";
+        public static void Main(string[] args)
+        {
+            int port = 21500;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start();
+
+            Console.WriteLine("Waiting for clients");
+            getClients(listener);
+
+            while (ServerSays != "quit") {
+                ServerSays = Console.ReadLine();
+            }
+        }
+
+        public async static void getClients(TcpListener listener)
+        {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine("New client found");
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+        }
+
+        public async static void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            int numberOfBytesRead = await stream.ReadAsync(buffer, 0, 256);
+            String receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+            Console.WriteLine(receivedMessage);
+
+           
+        }
+    }
+    
+}
---
+++ alle mine projekter/UdpAsyncClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpAsyncClientVersionOne
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 21000);
+
+            Task taskOne = sendMessage(client, endPoint);
+            taskOne.Wait();
+            Console.WriteLine("client shutting down");
+        }
+
+        public static async Task sendMessage(UdpClient client, IPEndPoint endPoint)
+        {
+            
+            // en bruger af chatten kan her sætte eget chatnavn
+            Console.WriteLine("Hello User. What is your name?");
+            String username = Console.ReadLine();
+
+            // brugernavn oversættes og sendes til server
+            byte[] translatedFromString = Encoding.UTF8.GetBytes(username + " says: ");
+            await client.SendAsync(translatedFromString, translatedFromString.Length, endPoint);
+
+            while (true)
+            {
+                String text = Console.ReadLine();
+                translatedFromString = Encoding.UTF8.GetBytes(text);
+                Console.WriteLine(text);
+                await client.SendAsync(translatedFromString, translatedFromString.Length, endPoint);
+            }
+        }
+    }
+}
