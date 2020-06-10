cf7eafb53ea69ea8f386d2ab3d0e12bcc79d6a87
Rene Christensen <rene723@gmail.com>
2020-06-10 08:28:29 +0200

    Async client and file put together in map and comments added

--- Endelig version/Async client and folder/Client/Program.cs
+++ Endelig version/Async client and folder/Client/Program.cs
@@ -2,28 +2,39 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace awaitClient
+namespace SimpleClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Vi sætter vores egen socket op. Samtidigt får vi adgang til metoder der gør det muligt at sende og modtage data
             TcpClient client = new TcpClient();
 
+            // declarerer simple typer
+            string text = "";
+            bool done = false;
+
+            // så sætter vi et endpoint op indholdende addressen på en anden socket som vi gerne vil sende data til. I dette tilfælde
+            // vil det være vores clientServer
             int port = 21000;
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             IPEndPoint endPoint = new IPEndPoint(ip, port);
-            string text = "";
+
+            // Vi skaber forbindelse til vores endpoint, altså serveren. Herefter får vi adgang til netværksstrømmen
             client.Connect(endPoint);
             NetworkStream stream = client.GetStream();
-            bool done = false;
+            
 
             Console.Write("Write your message or wait for a message from server \n");
 
-
+            // ReceiveMessage kører asynkront og tager løbende imod data fra vores klient, altså serveren når denne sender data over
+            // netværket.
             RecieveMessage(stream, client);
+
+            // Alt der skrives i terminalen sendes løbende over netværket til serveren ved hjælp af netværksstrømmen. Når klienten er
+            // færdig kan denne lukke ned for både klientsiden og serversiden ved at skrive "done"
             while (!done)
             {
                 if (text != "done")
@@ -49,6 +60,8 @@ namespace awaitClient
             client.Close();
         }
 
+        // vi aflæser datastrømmen og afventer at der kommer et relevant input. Dette input gemmes some en bytearray.
+        // ByteArrayen oversættes herefter til tekstform og udskrives hvis det ikke er en "end" kommando. Såfremt den er længere end 0.
         public static async void RecieveMessage(NetworkStream stream, TcpClient client)
         {
             byte[] buffer = new byte[256];
@@ -66,17 +79,21 @@ namespace awaitClient
                 else
                 {
                     Console.WriteLine("Server message: " + receivedMessage);
+
+                    // Det her er en måde at stoppe klienten fra serversiden. Det gøres ved at "lukke" vores socket (ved at sende strengen "end")
+                    // og afbryde datastrømmen. Herefter fejler client.connected(); tjekket i main og programmet stopper efterfølgende. 
                     stream.Close();
                     client.Close();
                     done = true;
-                    Console.WriteLine("Hit enter to close the client");
                 }
             }
         }
     }
 }
 
-
+// I forhold til simple response bonus opgaverne kan man selv vælge hvilke beskeder der skal kunne sendes og modtages.
+// Der er ikke lavet en samlet version af klient og server som sådan, men både klient og server er sat op til at kunne sende og modtage
+// beskeder løbende. 
 
 
 
--- Endelig version/Async client and folder/asyncServer/Program.cs
+++ Endelig version/Async client and folder/asyncServer/Program.cs
@@ -10,30 +10,39 @@ namespace AsyncServer
     {
         static void Main(string[] args)
         {
-            int port = 13358;
+            // Declarationer, oprettelse af endpoint og listener
+            string text = "";
+            bool done = false;
+
+            int port = 21000;
             IPAddress ip = IPAddress.Any;
             IPEndPoint localEndpoint = new IPEndPoint(ip, port);
 
             TcpListener listener = new TcpListener(localEndpoint);
-            string text = "";
 
-            bool done = false;
+            // vi sætter vores TcpListener til at vente på indkommenende forbindelser fra klientsiden
             listener.Start();
 
             Console.WriteLine("Await Clients");
 
+            // Vi opretter en task og beder tråden forholde sig afventende indtil programmet er udført og der er oprettet en tcpClient
+            // til kommunikation med klienten 
             Task<TcpClient> asyncClient = acceptClient(listener);
             asyncClient.Wait();
             TcpClient client = asyncClient.Result;
+
 
             NetworkStream stream = client.GetStream();
 
             Console.Write("Write your message or wait for a message from client \n");
 
             //ReceiveMessage(stream);
+
+            // Oversætter servermeddellelser til byteform og sender dem over netværksstrømmen til klienten. Meddellelsen "end" afslutter
+            // både klient(der er kode sat op til dette på klientsiden) og server
             while (!done)
             {
-                if (text != "done")
+                if (text != "end")
                 {
                     text = Console.ReadLine();
                     byte[] buffer = Encoding.UTF8.GetBytes(text);
@@ -46,16 +55,32 @@ namespace AsyncServer
                 }
             }
             Console.WriteLine("Hit enter to close the server");
+            Console.ReadLine();
         }
 
+        // Metoden returnerer en task der afventer kommunikationer fra klienter. Herudfra oprettes der en TcpClient.
+        // Ved at returnere en task kan vi bede den om at returnere resultatet til en funktion der ikke er asynkron (main)
+        // hvor vi dog skal være opmærksomme på ikke at tilgå resultatet får opgaven er udført. Man kan derfor med fordel
+        // kalde wait() metoden der stopper tråden til tasken er færdig og har returneret et resultat.
+        // Vi kalder også ReceiveMessage der modtager og udskriver beskeder fra klienten. Dette kan også gøres fra main
         public static async Task<TcpClient> acceptClient(TcpListener listener)
         {
             TcpClient client = await listener.AcceptTcpClientAsync();
             NetworkStream stream = client.GetStream();
             ReceiveMessage(stream);
+            // tilføjet fordi det var en bonusopgave at få serveren til at sende en besked efter klienten forbandt.
+            sendFirstServerMessage(stream);
             return client;
         }
 
+        // Oversætter en simpel string til byteform og sender den til klienten
+        public static void sendFirstServerMessage(NetworkStream stream)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes("Welcome Klient, to the server of Wonders!!");
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        // Denne metode er beskrevet i client og er endvidere givet. 
         public static async void ReceiveMessage(NetworkStream stream)
         {
             byte[] buffer = new byte[256];
@@ -76,3 +101,5 @@ namespace AsyncServer
         }
     }
 }
+
+// bonusopgave fra simpel call response løst. Vi sender en start besked til klienten.
