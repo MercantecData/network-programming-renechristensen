680b277f54b41b365703ccadaaa7a53353f28a1e
Rene Christensen <rene723@gmail.com>
2020-06-10 14:42:16 +0200

    Udp files saved in seperate folder, and comments added

--- Endelig version/Udp/UDPMultipleAsyncClient/Program.cs
+++ Endelig version/Udp/UDPMultipleAsyncClient/Program.cs
@@ -15,53 +15,54 @@ namespace UdpAsyncMultiClientServerVersionOne
          
         public static void Main(string[] args)
         {
+            // sætter en client op med tilhørende endpoint 
             int port = 21000;
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
             UdpClient client = new UdpClient(endPoint);
 
+            // metodekald
             receiveMessage(client, endPoint);
 
+            // send beskeder ud til alle klienter, afslut server med kommandoen "end"
             while (true)
             {
                 String serverMessage = Console.ReadLine();
                 sendMessageToAllClients(serverMessage, client);
+                if (serverMessage == "end")
+                {
+                    break;
+                }
             }
         }
 
+        
         public async static void receiveMessage(UdpClient client, IPEndPoint clientEndPoint)
         {
             byte[] buffer;
 
             while (true)
             {
+                // afvent beskeder fra klienter
                 UdpReceiveResult result = await client.ReceiveAsync();
+
+                // gem bytes og oversæt til tekst
                 buffer = result.Buffer;
                 String text = Encoding.UTF8.GetString(buffer);
                 Console.WriteLine(text);
+
+                // er det indkommende endpoint på endpointlisten? 
                 if(!endPointList.Contains(result.RemoteEndPoint))
                 {
                     endPointList.Add(result.RemoteEndPoint);
                 }
-                sendMessageToAllClients(text, result.RemoteEndPoint, clientEndPoint, client);
-                
-            }
-        }
-
-        public static void sendMessageToAllClients(String message, IPEndPoint foreignEndPoint, IPEndPoint clientEndPoint, UdpClient client)
-        {
-            foreach (IPEndPoint endPoint in endPointList)
-            {
-
-                if (!(endPoint.Address.Equals(foreignEndPoint.Address)))
-                {
-                    byte[] translatedFromString = Encoding.UTF8.GetBytes(message);
-                    client.Send(translatedFromString, translatedFromString.Length, endPoint);   
-                }
+                // send beskeder til alle klienter
+                sendMessageToAllClients(text, client);
             }
         }
 
         public static void sendMessageToAllClients(String message, UdpClient client)
         {
+            // oversæt message til byteform og send til hver client
             foreach (IPEndPoint endPoint in endPointList)
             {
                 byte[] translatedFromString = Encoding.UTF8.GetBytes(message);
